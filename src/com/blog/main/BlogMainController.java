82b852ce97ec7b3354ab43a2cf7c2692fe63fb0a
Cheng Xuegang <chxn5450036@126.com>
2017-07-02 11:21:17 +0800

    提交jar

---
+++ src/com/blog/main/BlogMainController.java
@@ -0,0 +1,13 @@
+package com.blog.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class BlogMainController {
+	@RequestMapping("/main")
+	public String mainPage(){
+		return "main";
+	}
+}
