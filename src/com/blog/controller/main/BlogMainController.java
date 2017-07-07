973984bbf0c7e6ca2ea3e27941e7f24f6d08b75f
chengxuegang <chxn5450036@126.com>
2017-07-07 17:47:52 +0800

    日终提交

--- src/com/blog/controller/main/BlogMainController.java
+++ src/com/blog/controller/main/BlogMainController.java
@@ -1,10 +1,14 @@
 package com.blog.controller.main;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.rex.RMap;
 import org.rex.db.logger.Logger;
 import org.rex.db.logger.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +19,7 @@ import com.blog.service.main.MainService;
 public class BlogMainController {
 	Logger logger = LoggerFactory.getLogger(BlogMainController.class);
 	
+	 @Autowired
 	private MainService mainservice;
 	@RequestMapping("/main")
 	public String mainPage(){
@@ -24,6 +29,13 @@ public class BlogMainController {
 	@RequestMapping("/getTopTewBlog")
 	public ModelAndView getTopNewBlog(HttpServletRequest request,
 	         HttpServletResponse response) throws Exception {
-		List<RMap> blogList = 
+		RMap params = new RMap();
+		List<RMap> blogList = this.mainservice.getTopNewBlog(params);
+		//∑µªÿmodelAndView
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("blogList", blogList);
+		//÷∏∂® ‘Õº
+		modelAndView.setViewName("blog_index");
+		return modelAndView;
 	}
 }
